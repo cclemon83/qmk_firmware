d86305895d8d92683b69de6cdda0edce039c27b7
DESKTOP\daisuke <apwjgm@gmail.com>
2022-04-28 13:53:19 +0900

    New addition

---
+++ keyboards/crkbd/keymaps/cclemon/rules.mk
@@ -0,0 +1,11 @@
+MOUSEKEY_ENABLE = yes     # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+VIA_ENABLE      = yes    # Enable VIA
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+POINTING_DEVICE_ENABLE 	    = yes
+POINTING_DEVICE_DRIVER      = analog_joystick
+NKRO_ENABLE = yes
+# BOOTLOADER                  = qmk-dfu
+SRC += analog.c
